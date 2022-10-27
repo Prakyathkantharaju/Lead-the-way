9c7f088394428b31681bf9a163f27229764834c4
Prakyath Kantharaju <prakyathk33@gmail.com>
2022-10-27 17:37:47 -0500

    Testing the movement

--- drone_control/test_control.py
+++ drone_control/test_control.py
@@ -1,13 +1,41 @@
 from djitellopy import Tello
+from human_direction_simple import HumanMovementCommand
+import numpy as np
+
 
 tello = Tello(retry_count=10)
+hmc = HumanMovementCommand()
 
 tello.connect()
 
 tello.takeoff()
 
-# tello.move_left(100)
+Takeoff = True
 
-tello.rotate_counter_clockwise(90)
+while Takeoff:
+
+    hmc.get_command()
+
+    if hmc.STOP:
+        tello.land()
+        hmc.START = False
+        Takeoff = False
+
+
+    if hmc.forward > 0:
+        print("forward")
+        tello.move_forward(100)
+
+    if hmc.Rotate > 0:
+        print("Rot")
+        tello.rotate_clockwise(10)
+    
+    if hmc.Rotate < 0:
+        print("Anti Rot")
+        tello.rotate_counter_clockwise(10)
+
+    if hmc.sideways > 0:
+        tello.move_forward(80)
+
 
 tello.land()
--- get_similarity/tello_hand_movements.py
+++ get_similarity/tello_hand_movements.py
@@ -5,7 +5,8 @@ from Graph import Graph_structure
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
-from threading import Thread
+import skvideo.io
+from tqdm import tqdm
 
 G = Graph_structure()
 hmc = HumanMovementCommand()
@@ -17,56 +18,23 @@ tello.connect()
 keepRecording = True
 tello.streamon()
 frame_read = tello.get_frame_read()
-
-def videoRecorder():
-    # create a VideoWrite object, recoring to ./video.avi
-    height, width, _ = frame_read.frame.shape
-    
-    fig, ax = plt.subplots(1,1)
-
-    while keepRecording:
-        ax.cla()
-        frame = frame_read.frame
-        # G.add_images(frame, [hmc.forward, hmc.sideways, hmc.Rotate])
-        # nx.draw(G.Graph, ax = ax, with_labels = True)
-        # print(frame.shape)
-        # fig.canvas.draw()
-        # data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-        # data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-
-        # data = cv2.resize(data, (480, 852), interpolation=cv2.INTER_AREA)
-
-        # frame = cv2.resize(frame, (480, 852), interpolation=cv2.INTER_AREA)
-
-        # frame_1 = np.concatenate((frame, data), axis = 1)
-        print(frame.shape)
-        video.write(frame)
-        time.sleep(1/10)
-        cv2.imshow('frame', frame)
-      
-        # the 'q' button is set as the
-        # quitting button you may use any
-        # desired button of your choice
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    video.release()
+Takeoff = False
 
 
-# grapher = Thread(target=videoRecorder)
 
-# grapher.start()
-
-# grapher.join()
 fig, ax = plt.subplots(1,1)
 
 height, width, _ = frame_read.frame.shape
-video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
-for i in range(3000):
+nx.draw(G.Graph, ax=ax, with_labels=True)
+video_data = []
+video_data_storage = []
+for i in tqdm(range(100)):
     hmc.get_command()
     frame = frame_read.frame
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     if i % 15 == 0:
         G.add_images(frame, [1,0,0])
+        nx.draw(G.Graph, ax=ax, with_labels=True)
     fig.canvas.draw()
     data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
     data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
@@ -76,14 +44,41 @@ for i in range(3000):
     frame = cv2.resize(frame, (480, 852), interpolation=cv2.INTER_AREA)
 
     frame_1 = np.concatenate((frame, data), axis = 1)
-    print(frame_1.shape)
-    video.write(frame)
+    
+    if hmc.START:
+
+        if not Takeoff:
+            tello.takeoff()
+            Takeoff = True
+
+        if hmc.STOP:
+            tello.land()
+            hmc.START = False
+            Takeoff = False
+
+
+        if hmc.forward > 0:
+            tello.move_forward(hmc.forward * 50)
+
+        if hmc.Rotate > 0:
+            tello.rotate_clockwise(np.rad2deg(hmc.Clock_wise))
+        
+        if hmc.Rotate < 0:
+            tello.rotate_counter_clockwise(np.rad2deg(hmc.Anti_Clock_wise))
+
+        if hmc.sideways > 0:
+            tello.move_forward(hmc.forward * 20)
+
+    # if i == 0:
+    #     video_data = frame_1[np.newaxis, :]
+    # else:
+    #     video_data = np.concatenate((video_data, frame_1[np.newaxis,:]), axis=0)
+    video_data.append(frame_1)
+    
     time.sleep(1/30)
-    plt.imsave(f"tmp/{i}.png",frame_1)
 
 
-video.release()
-
+skvideo.io.vwrite("test_live_video.mp4", np.array(video_data))
 nx.draw(G.Graph)
 plt.show()
     
