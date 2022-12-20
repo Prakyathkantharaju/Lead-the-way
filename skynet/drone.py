6a094b4266b9d7095f84afa9c7e836bbf3a6b2fb
Prakyath Kantharaju <prakyathk33@gmail.com>
2022-12-20 07:28:32 -0600

    skynet client update

--- skynet/drone.py
+++ skynet/drone.py
@@ -1,19 +1,18 @@
-import numpy as np
 import pickle
-import torch
-from efficientnet_pytorch import EfficientNet
-import torchvision.transforms as transforms
+import numpy as np
 from PIL import Image
 import networkx as nx
+import torch
+from torchvision import transforms
+from efficientnet_pytorch import EfficientNet
 
 
-
-class DiffCar(object):
+class Drone:
     def __init__(self, config) -> None:
         self.config = config
         self.data_store = config['data_store']
         self.model = EfficientNet.from_pretrained('efficientnet-b0')
-        self.tranformation = transforms.Compose([transforms.Resize(224), 
+        self.tranformation = transforms.Compose([transforms.Resize(224),
                                                  transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
         self.cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
@@ -26,22 +25,25 @@ class DiffCar(object):
         im_vev = torch.tensor(self.model.extract_features(img_transformed).view(-1)).unsqueeze(0)
         return im_vev
 
-    def _update_graph(self, features: torch.Tensor, data:dict, 
-                      robot_name:str = "diff_car") -> None:
+    def _update_graph(self, features: torch.Tensor, data:dict,
+                      robot_name:str = "drone") -> None:
         id = self.Graph.number_of_nodes()
         self.Graph.add_node(id, features=features, location=data['location'],
                             image = data['image'])
         # if not starting node please add edge
         if self.Graph.number_of_nodes() > 1:
-            self.Graph.add_edge(id, id - 1, robot_name=robot_name, 
+            self.Graph.add_edge(id, id - 1, robot_name=robot_name,
                                 command = data['command'],
                                 speed = data['speed'])
+    
 
     def processing_function(self, msg):
-        # processing function
         data = pickle.loads(msg)
         image = data['image']
-        # get features from the image
+        # get features from the Image
         features = self._get_features(image)
         self._update_graph(features, data)
-        
+
+
+
+
---
+++ skynet/sqlite_helper.py
@@ -0,0 +1,48 @@
+import sqlite3
+import pickle
+import numpy as np
+from typing import List
+
+class PickleSQLiteHelper:
+    def __init__(self, db_name: str):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+
+    def create_table(self, table_name: str):
+        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY, image BLOB, location BLOB, pickle BLOB)")
+        self.conn.commit()
+
+    def insert_pickle(self, table_name, id: int, image: np.ndarray, location: List[int], pickle_obj: bytes):
+        # Convert the image and location to binary strings
+        image_str = image.tobytes()
+        location_str = pickle.dumps(location)
+        # Convert the pickle object to a binary string
+        pickle_str = pickle.dumps(pickle_obj)
+        # Insert the binary strings into the table as blobs
+        self.cursor.execute(f"INSERT INTO {table_name} VALUES (?, ?, ?, ?)", (id, image_str, location_str, pickle_str))
+        self.conn.commit()
+        return self.cursor.lastrowid
+
+    def retrieve_pickle(self, table_name: str, id: int):
+        self.cursor.execute(f"SELECT image, location, pickle FROM {table_name} WHERE id=?", (id,))
+        # Retrieve the image, location, and pickle object from the binary strings
+        row = self.cursor.fetchone()
+        image = np.frombuffer(row[0], dtype=np.uint8).reshape((1, -1))
+        location = pickle.loads(row[1])
+        pickle_obj = pickle.loads(row[2])
+        return image, location, pickle_obj
+
+
+
+    def retrieve_pickles_in_range(self, table_name: str, min_location: int, max_location: int):
+        self.cursor.execute(f"SELECT image, location, pickle FROM {table_name} WHERE location BETWEEN ? AND ?", (min_location, max_location))
+        rows = self.cursor.fetchall()
+        # Retrieve the image, location, and pickle object for each row
+        results = []
+        for row in rows:
+            image = np.frombuffer(row[0], dtype=np.uint8).reshape((1, -1))
+            location = pickle.loads(row[1])
+            pickle_obj = pickle.loads(row[2])
+            results.append((image, location, pickle_obj))
+        return results
+
